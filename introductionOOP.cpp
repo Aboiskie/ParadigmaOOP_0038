4cd1bf1830576e5f145d86e6001571c409b88e36
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-04-20 10:28:22 +0700

    menambahkan metode printData() di kelas Mahasiswa

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -8,4 +8,12 @@ class Mahasiswa{
         string nama;
         float nilai;
 
-       
+        void printData(){
+            cout << "NIM: " << nim << endl;
+            cout << "Nama: " << nama << endl;
+            cout << "Nilai: " << nilai << endl;
+        }
+}; //batas class
+
+int main(){
+   
